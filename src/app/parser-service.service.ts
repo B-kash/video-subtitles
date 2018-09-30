fa71da7670e5be4ecdc89e50f7b64ea2e308614d
Bikash Chapagain <bikash.chapagain@yco.com.np>
2018-09-30 12:57:56 +0545

    added functions to get cue of particular time, added function to reset cues if source change is clicked and added its tests,

--- src/app/parser-service.service.ts
+++ src/app/parser-service.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class ParserServiceService {
-
+export class ParserService {
+  cues:any = [];
   constructor() { }
+  getAllCues(){
+    return this.cues;
+  }
+  reset(){
+    this.cues = [];
+//    TODO now download another subtitle again and send it to parse into above functions
+  }
+  getCueForTime(currentTime) {
+    return this.cues.filter(cue=> currentTime>=cue.startDuration && currentTime<cue.endDuration);
+  }
+
+
 
 }
